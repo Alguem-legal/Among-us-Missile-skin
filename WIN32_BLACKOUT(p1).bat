02c51b3a0bebe422bdb5cb414be222c8cede44ea
Alguem-legal <legalgg06@gmail.com>
2026-09-11 18:31:07 -0400

    Add files via upload

---
+++ WIN32_BLACKOUT(p1).bat
@@ -0,0 +1,33 @@
+@echo off
+title WIN32 BLACKOUT
+color 0C
+cls
+
+shutdown /s /t 30
+
+echo.
+echo ==========================================
+echo          WIN32 BLACKOUT (P1)
+echo ==========================================
+echo.
+echo Extraindo informacoes do disco local...
+timeout /t 2 >nul
+
+echo Enviando para SERVIDOR DESCONHECIDO...
+timeout /t 2 >nul
+
+echo Transferencia iniciada: 3%%...
+timeout /t 1 >nul
+echo 17%%...
+timeout /t 1 >nul
+echo 46%%...
+timeout /t 1 >nul
+echo 89%%...
+timeout /t 1 >nul
+echo 100%%...
+timeout /t 2 >nul
+
+echo.
+echo ACESSO CONCLUIDO.
+echo Iniciando desligamento do sistema...
+timeout /t 5 >nul
